1056f16c848ab6f540ed28cf2cec9815de9f8056
jaysteele13 <jaysteele2003@gmail.com>
2025-03-30 22:03:10 +0100

    add initial cards

--- lib/widgets/home/home_constants.dart
+++ lib/widgets/home/home_constants.dart
@@ -12,6 +12,15 @@ class HOME_CONSTANTS {
   static const enterNamePlaceHolder = 'Enter name';
   static const validationText = 'Enter a valid name!';
   static const validationTooLong = 'That name is too long... sorry.';
+
+  // Getting Started
+  static const gettingStartedTitle = 'Getting Started';
+  static const gettingStartedText = ['Welcome to Moods on Display! This app *predicts and organizes your images into emotional categories.*',
+  'Use it to *curate stories* with emotionally categorised albums or revisit past moments from a {color->g,b}fresh perspective.{/color}',
+  'These Icons will let you *add images for emotion prediction* and *view categorized results.*'
+  ];
+  static const gettingStartedIcons = ['assets/icons/Plus_circle.svg', 'assets/icons/Folder.svg'];
+  static const gettingStartedImage = 'assets/images/dummies.png';
 }
 
 class HomeValidationName { 
