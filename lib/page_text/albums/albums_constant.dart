afe64c5beee3858d52917699caaefb4e2a9f6741
jaysteele13 <jaysteele2003@gmail.com>
2025-04-12 21:17:39 +0100

    added info

--- lib/page_text/albums/albums_constant.dart
+++ lib/page_text/albums/albums_constant.dart
@@ -1,5 +1,14 @@
 class ALBUMS_CONSTANTS {
 
   static const title = "Mood Albums";
+
+  static const infoTitle = "This page outlines the moods you’ve captured";
+static const infoParagraphs = [
+  "Each folder represents a specific {color->g,b}mood{/color}.",
+  "7 Emotions are calculated by my AI model, which sums to *100%* overall.",
+  "Images are categorized based on the {color->y,b,u}highest average emotion score{/color}.",
+  "Some faces may not *perfectly match* the core emotion due to {color->D,u}model limitations{/color}.",
+];
+
 }
 
